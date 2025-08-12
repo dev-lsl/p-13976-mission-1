62b742008274efb51139c3ade0b5bcd278a22ba0
dev-lsl <sanglim7698@gmail.com>
2025-08-12 12:35:38 +0900

    명언 ëªã록, 삭제, 수정 메서드 구현

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -38,4 +38,55 @@ object Main {
         wiseSayingsList.add(WiseSayings(id, wiseSay, author))
         println("$id 번 명언 등록 완료")
     }
+
+    private fun handleList() {
+        println("번호 /  작가 / 명언")
+
+        for (w in wiseSayingsList) {
+            println("${w.id} / ${w.author} / ${w.wiseSay}")
+        }
+
+    }
+    // 삭제?id=1
+    private fun handleDelete(cmd: String) {
+        val idToDelete = cmd.substring("삭제?id=".length).toIntOrNull() // id 추출
+
+        var found = false
+
+        for (w in wiseSayingsList) {
+            if (w.id == idToDelete) {
+                wiseSayingsList.remove(w)
+                println("$idToDelete 번 명언 삭제 완료")
+                found = true
+                break
+            }
+        }
+        if (!found) {
+            println("$idToDelete 번 명언은 존재하지 않습니다.")
+        }
+
+    }
+
+    private fun handleUpdate(cmd: String) {
+        val idToUpdate = cmd.substring("수정?id=".length).toIntOrNull() // id 추출
+
+        var found = false
+
+        for (w in wiseSayingsList){
+            if (w.id== idToUpdate) {
+
+                println("명언(기존) : ${w.wiseSay}")
+                print("명언 : ")
+                w.wiseSay = scanner.nextLine().trim()
+
+                println("작가(기존) : ${w.author}")
+                print("작가 : ")
+                w.author = scanner.nextLine().trim()
+
+                println("$idToUpdate 번 명언 수정 완료")
+                found = true
+                break
+            }
+        }
+    }
 }
